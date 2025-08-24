3a64ea9e4c54b79efa66f59a24a5a8c048d23f89
22-luis <0006720@uca.edu.sv>
2025-08-24 14:55:11 -0600

    Add: views

--- Blog/models.py
+++ Blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Post(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+
+from Blog.models import Post
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    latest_post_list = Post.objects.order_by("date")[:5]
+    context = {"latest_post_list": latest_post_list}
+    return render(request, "blog/index.html", context)
+
+def detail(request, post_id):
+    post  = get_object_or_404(Post, pk=post_id)
+    return render(request, "blog/detail.html", {"post": post})
+
+def content(request, post_id):
+    response = "You are looking at task %s."
+    return HttpResponse(response % post_id)
